e3fada16e23ae142ed68851cdb1ae8ff010289d4
TomoyaTakayama <pengmigaoshan190@gmail.com>
2026-06-23 12:15:35 +0900

    first commit

---
+++ Sushikuinee.java
@@ -0,0 +1,25 @@
+package jp.ac.hal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sushikuinee {
+
+	public static void main(String[] args) throws IOException
+	{
+		
+		
+		
+		BufferedReader br =
+				new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("いらっしゃいませ！\n何名様ですか？");
+		
+		int member = Integer.parseInt(br.readLine());
+		
+		
+		
+	}
+
+}
